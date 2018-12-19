9126f0959d8805e9a79c1fbc12ad59778b7835a3
Angelo Corsaro <angelo@icorsaro.net>
2018-12-19 23:29:43 +0100

    exposed codec for values

--- lib/yaks_fe_sock_codec.mli
+++ lib/yaks_fe_sock_codec.mli
@@ -1,6 +1,8 @@
 open Apero
 open Yaks_fe_sock_types
 
-val encode_message : message -> IOBuf.t -> (IOBuf.t, Atypes.error) result
+val encode_value : Yaks_types.Value.t -> IOBuf.t -> (IOBuf.t, Atypes.error) result
+val decode_value : IOBuf.t -> (Yaks_types.Value.t * IOBuf.t, Atypes.error) result
 
+val encode_message : message -> IOBuf.t -> (IOBuf.t, Atypes.error) result
 val decode_message : IOBuf.t -> (message * IOBuf.t, Atypes.error) result
