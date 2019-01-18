2121cfeb9fe391b3fb144f13f6b4d3547611fc03
Julien Enoch <946462+JEnoch@users.noreply.github.com>
2019-01-18 14:37:06 +0100

    Use Apero's Path and PathExpr, updating Selector API. Resolves atolab/yaks#5

--- lib/yaks_types.ml
+++ lib/yaks_types.ml
@@ -2,60 +2,17 @@ open Apero
 open Yaks_common_errors
 
 
-let remove_useless_slashes s =
-  let slashes = Str.regexp "/+" in
-  let s' = Str.global_replace slashes "/" s in
-  let l = Astring.length s' in
-  if l>1 && (Astring.get s' (l-1) = '/') then Astring.with_range ~len:(l-1) s' else s'
 
-module Path = struct
-  type t = string
-  (* from https://tools.ietf.org/html/rfc3986#appendix-B :
-     the path of a URI contains any char but '?' or '#'.
-     We add '*', '[' and ']' as forbidden char *)
-  let path_regex = Str.regexp "^[^][?#*]+$"
-
-  let is_valid s = Str.string_match path_regex s 0
-
-  let of_string_opt s =
-    let s = Astring.trim s in
-    if is_valid s then Some (remove_useless_slashes s) else None
-
-  let of_string s =
-    Apero.Option.get_or_else (of_string_opt s)
-    (fun () -> raise (YException (`InvalidPath (`Msg s))))
-
-  let to_string s = s
-
-  let length = Astring.length
-
-  let compare = Astring.compare
-
-  let is_relative p = Astring.get p 0 <> '/'
-
-  let add_prefix ~prefix p = remove_useless_slashes @@ (to_string prefix)^"/"^p
-
-  let is_prefix ~affix path = Astring.is_prefix ~affix:(to_string affix) (to_string path)
-
-  let remove_prefix length path = Astring.after length (to_string path) |> of_string
-end [@@deriving show]
+module Path = Apero.Path
 
 module Selector = struct
 
-  type t = { path: string; pred: string option; props: string option; frag: string option }
+  type t = { path: PathExpr.t; pred: string option; props: string option; frag: string option }
 
-
-  (* from https://tools.ietf.org/html/rfc3986#appendix-B except that:
-      - we don't allow '[' or ']' in the path part
-      - the query part (after '?') is optional, but if set it must be composed of
-         - an optional predicate part not containing any '(' or ')'
-         - an optional properties part enclosed between '(' and ')'
-    TODO: constrain regex for predicate, props and fragment (i.e. specify the separators such as '&', '='...)
-   *)
 
   let sel_regex =
-    let path = "[^?#]+" in
-    let predicate = "[^()#]+" in
+    let path = "[^][?#]+" in
+    let predicate = "[^][()#]+" in
     let properties = ".*" in
     let fragment = ".*" in
     Str.regexp @@ Printf.sprintf "^\\(%s\\)\\(\\?\\(%s\\)?\\((\\(%s\\))\\)?\\)?\\(#\\(%s\\)\\)?$" path predicate properties fragment
@@ -66,13 +23,14 @@ module Selector = struct
   let of_string_opt s =
     let s = Astring.trim s in
     if is_valid s then
+      match PathExpr.of_string_opt @@ Str.matched_group 1 s with
+      | Some path ->
         let pred = try Some(Str.matched_group 3 s) with Not_found -> None
         and props = try Some(Str.matched_group 5 s) with Not_found -> None
         and frag = try Some(Str.matched_group 7 s) with Not_found -> None
-        and path = remove_useless_slashes @@ Str.matched_group 1 s
-        (* Note: path computed at the end as remove_useless_slashes also uses Str and will make Str.matched_group misbehave *)
         in
         Some { path; pred; props; frag }
+      | None -> None
     else None
 
   let of_string s =
@@ -81,15 +39,16 @@ module Selector = struct
 
 
   let to_string s =
-    Printf.sprintf "%s%s%s%s%s" s.path 
+    Printf.sprintf "%s%s%s%s%s"
+      (PathExpr.to_string s.path)
       (if Option.is_some s.pred || Option.is_some s.props then "?" else "")
       (match s.pred with | Some(q) -> q | None -> "")
       (match s.props with | Some(p) -> "("^p^")" | None -> "")
       (match s.frag with | Some(f) -> "#"^f | None -> "")
 
-  let of_path p = of_string @@ Path.to_string p
+  let of_path p = { path=PathExpr.of_path p; pred=None; props=None; frag=None }
 
-  let path s = s.path
+  let path s = PathExpr.to_string s.path
 
   let predicate s = s.pred
 
@@ -97,140 +56,26 @@ module Selector = struct
 
   let fragment s = s.frag
 
-  let is_relative sel = Astring.get sel.path 0 <> '/'
+  let is_relative sel = PathExpr.is_relative sel.path
 
-  let add_prefix ~prefix sel =
-    { path = remove_useless_slashes @@ (Path.to_string prefix)^"/"^sel.path;
-      pred = sel.pred; props = sel.props; frag = sel.frag }
+  let add_prefix ~prefix sel = { sel with path = PathExpr.add_prefix ~prefix sel.path }
 
-  let is_path_unique sel = not @@ Astring.contains '*' (path sel)
+  let get_prefix sel = PathExpr.get_prefix sel.path
 
-  let as_unique_path sel = if is_path_unique sel then Some (Path.of_string @@ path sel) else None
+  let is_path_unique sel = PathExpr.is_unique sel.path
 
-  let simple_wildcard = Astring.Sub.v "*"
-  let double_wildcard = Astring.Sub.v "**"
-  let non_wildcard c = c != '*'
-  let is_slash c = c = '/'
+  let as_unique_path sel = PathExpr.as_unique_path sel.path
 
-  let get_prefix_before_wildcard s = Astring.Sub.take ~sat:non_wildcard s
+  let is_matching_path pat sel = PathExpr.is_matching_path pat sel.path
 
-  let get_prefix_before_wildcard_until_slash s = 
-    let open Astring.Sub in 
-    let prefix = take ~sat:non_wildcard s in
-    match find is_slash prefix with
-    | Some slash -> with_index_range ~first:(start_pos prefix) ~last:(start_pos slash) @@ base prefix
-    | None -> prefix
+  let includes ~subsel sel = PathExpr.includes ~subexpr:subsel.path sel.path
 
-  let get_prefix_until_slash s =
-    let open Astring.Sub in 
-    match find is_slash s with
-    | Some slash -> with_index_range ~first:(start_pos s) ~last:(start_pos slash) @@ base s
-    | None -> s
+  let intersects sel1 sel2 = PathExpr.intersect sel1.path sel2.path
 
-  let rec check_matching p s =
-    let open Astring.Sub in
-    (* if selector is empty, path must be empty too *)
-    if is_empty s then is_empty p
-    (* if selector starts with '**' *)
-    else if is_prefix ~affix:double_wildcard s then
-      (* if selector is only "**" it matches all (if any) *)
-      if length s <= 2 then not @@ is_empty p
-      else (
-        (* get the next wildcard-free sub-string from selector and find it in path *)
-        let sub = with_range ~first:2 s |> get_prefix_before_wildcard in
-        match find_sub ~rev:true ~sub p with
-        | None -> false
-        | Some sub' ->
-          (* substring found; go on with remaining parts *)
-          let pat_tail = with_range ~first:(stop_pos sub') (base sub') in
-          let sel_tail = with_range ~first:(2+(length sub)) s in
-          check_matching pat_tail sel_tail)
-    (* if selector starts with '*' *)
-    else if is_prefix ~affix:simple_wildcard s then
-      let _ = Logs_lwt.debug (fun m -> m "----- check * with %a and %a" pp s pp p) in
-      (* if selector is only '*', it matches path if doesn't contain a '/' *)
-      if length s <= 1 then let _ = Logs_lwt.debug (fun m -> m "-------- last char is *") in (not @@ is_empty p) && (not @@ exists is_slash p)
-      else (
-        (* get the next wildcard-free and slash-free sub-string from selector and find it in next path' segment *)
-        let sub_sel = with_range ~first:1 s |> get_prefix_before_wildcard_until_slash in
-        let sub_pat = get_prefix_until_slash p in
-        let _ = Logs_lwt.debug (fun m -> m "-------- sub_sel=%a and sub_path=%a" pp sub_sel pp sub_pat) in
-        match find_sub ~sub:sub_sel sub_pat with
-        | None -> let _ = Logs_lwt.debug (fun m -> m "-------- sub_sel not found in sub_pat") in false
-        | Some sub' ->
-          (* substring found; go on with remaining parts *)
-          let pat_tail = with_range ~first:(stop_pos sub') (base sub') in
-          let sel_tail = with_range ~first:(1+(length sub_sel)) s in
-          let _ = Logs_lwt.debug (fun m -> m "-------- sub_sel found in sub_pat . Go on with %a and %a" pp sel_tail pp pat_tail) in
-          check_matching pat_tail sel_tail)
-    (* selector doesn't start with wildcard *)
-    else
-      (* get the next wildcard-free sub-string from selector and check is path starts with this *)
-      let sub = get_prefix_before_wildcard s in
-      if is_prefix ~affix:sub p then
-        (* path starts with substring; go on with remaining parts *)
-        let first = length sub in
-        check_matching (with_range ~first p) (with_range ~first s)
-      else false
-
-  let is_matching_path pat sel =
-    let open Astring.Sub in
-    let sel' = v @@ path sel in
-    let pat' = v @@ Path.to_string pat in
-    let result = check_matching pat' sel' in
-    let _ = Logs_lwt.debug (fun m -> m "[Yco] Selector.is_matching_path %a %a : %b" pp pat' pp sel' result) in
-    result
-
-  let is_including_selector ~subsel sel =
-    let open Astring.Sub in
-    let sel' = v @@ path sel in
-    let subsel' = v @@ path subsel in
-    let result = check_matching subsel' sel' in
-    let _ = Logs_lwt.debug (fun m -> m "[Yco] Selector.is_including_selector %a %a : %b" pp subsel' pp sel' result) in
-    result
-
-  let remove_matching_prefix pat sel =
-    let rec check_is_prefixed p s =
-      let open Astring.Sub in
-      (* if path is empty, it's indeeed a prefix of the selector, return the selector *)
-      if is_empty p then Some s
-      (* if selector is empty, there is no match, return None *)
-      else if is_empty s then None
-      (* if selector starts with '**', the path is indeed matching '**', return the selctor (incl. '**') *)
-      else if is_prefix ~affix:double_wildcard s then Some(s)
-      (* if selector starts with '*' *)
-      else if is_prefix ~affix:simple_wildcard s then
-        (* if path has no '/', '*' match the path, return the remaining of selector after its '*' *)
-        if not @@ exists is_slash p then Some(with_range ~first:1 s)
-        (* else, if the selector is only '*', the path is not matching *)
-        else if length s <= 1 then None
-        (* else, go on after consumption of '*' and the matching segment in path *)
-        else (
-          let sub_sel = with_range ~first:1 s |> get_prefix_before_wildcard_until_slash in
-          let sub_pat = get_prefix_until_slash p in
-          match find_sub ~sub:sub_sel sub_pat with
-          | None -> None
-          | Some sub' ->
-            let pat_tail = with_range ~first:(stop_pos sub') (base sub') in
-            let sel_tail = with_range ~first:(1+(length sub_sel)) s in
-            check_is_prefixed pat_tail sel_tail)
-      (* if selector doesn't start with wildcard, check if it's part before the first wildcard matches the path *)
-      else
-        let sub = get_prefix_before_wildcard s in
-        if is_prefix ~affix:p sub then Some(with_range ~first:(length p) s)
-        else if is_prefix ~affix:sub p then
-          let first = length sub in
-          check_is_prefixed (with_range ~first p) (with_range ~first s)
-        else None
-    in 
-    let sel' = Astring.Sub.v @@ path sel in
-    let pat' = Astring.Sub.v @@ Path.to_string pat in
-    let open Apero.Option.Infix in
-    let result = check_is_prefixed pat' sel' >|= Astring.Sub.to_string >>= of_string_opt in
-    let _ = Logs_lwt.debug (fun m -> m "[Yco] Selector.remove_matching_prefix %a %a : %s" Astring.Sub.pp pat' Astring.Sub.pp sel' (match result with Some s -> to_string s | None -> "None")) in
-    result
-
-  let is_prefixed_by_path path selector = remove_matching_prefix path selector |> Apero.Option.is_some
+  let remaining_after_match path sel =
+    match PathExpr.remaining_after_match path sel.path with
+    | Some remain -> Some { sel with path = remain }
+    | None -> None
 
 end
 
--- lib/yaks_types.mli
+++ lib/yaks_types.mli
@@ -1,30 +1,6 @@
 open Yaks_common_errors
 
-module Path : sig
-  type t
-  val of_string : string -> t
-  (** [of_string s] returns [s] as a Path if it's valid. Otherwise it raises a [YException].
-      Note that the Path's string is sanitized (i.e. it's trimmed meaningless '/' are removed) *)
-  val of_string_opt : string -> t option
-  (** [of_string_opt s] returns [Some p] if [s] is a valid path. Otherwise it returns [None].
-      Note that the Path's string is sanitized (i.e. it's trimmed and meaningless '/' are removed) *)
-  val to_string : t -> string
-  (** [to_string path] returns the [path] as a string. *)
-
-  val length : t -> int
-  (** [length p] returns the number of characters of path [p] *)
-  val compare : t -> t -> int
-  (** The comparison function for strings, with the same specification as [Pervasives.compare] *)
-
-  val is_relative : t -> bool
-  (** [is_relative p] return true if the Path [p] is relative (i.e. it's first character is not '/') *)
-  val add_prefix : prefix:t -> t -> t
-  (** [add_prefix prefix p] return a new Path made of [prefix]/[p] *)
-  val is_prefix : affix:t -> t -> bool
-  (** [is_prefix affix p] returns true if [affix] is a prefix of [p] *)
-  val remove_prefix : int -> t -> t
-  (** [remove_prefix l p] removes the [l] first characters from Path [p] and returns the remaining as a non-absolute Path *)
-end [@@deriving show]
+module Path = Apero.Path
 
 module Selector : sig
   type t          
@@ -43,11 +19,11 @@ module Selector : sig
   (** [path s] returns the path part of the Selector [s]. I.e. the part before any '?' character. *)
   val predicate : t -> string option
   (** [predicate s] returns the predicate part of the Selector [s].
-      I.e. the substring after the first '?' character and before the first '[' or '#' character (if any).
+      I.e. the substring after the first '?' character and before the first '(' or '#' character (if any).
       None is returned if their is no such substring. *)
   val properties : t -> string option
   (** [properties s] returns the properties part of the Selector [s].
-      I.e. the substring enclosed between '[' and']' and which is after the first '?' character and before the fist '#' character (if any) ., or an empty string if no '?' is found.
+      I.e. the substring enclosed between '(' and')' and which is after the first '?' character and before the fist '#' character (if any) ., or an empty string if no '?' is found.
       None is returned if their is no such substring. *)
   val fragment : t -> string option
   (** [fragment s] returns the fragment part of the Selector [s].
@@ -58,6 +34,9 @@ module Selector : sig
   val add_prefix : prefix:Path.t -> t -> t
   (** [add_prefix prefix s] return a new Selector with path made of [prefix]/[path s].
       The predicate, properties and fragment parts of this new Selector are the same than [s]. *)
+  val get_prefix : t -> Path.t
+  (** [get_prefix s] return the longest Path (i.e. without any wildcard) that is a prefix in [s]
+      The predicate, properties and fragment parts of [s] are ignored in this operation. *)
 
   val is_path_unique : t -> bool
   (** [is_path_unique s] returns true it the path part of Selector [s] doesn't contains any wildcard ('*'). *)
@@ -68,16 +47,16 @@ module Selector : sig
   val is_matching_path : Path.t -> t -> bool
   (** [is_matching_path p s] returns true if the selector [s] fully matches the path [p].
       Note that only the path part of the selector is considered for the matching (not the query and the fragment) *)
-  val is_including_selector : subsel:t -> t -> bool
-  (** [is_including_selector subsel s] returns true if the selector [s] includes the selector [subsel].
+  val includes : subsel:t -> t -> bool
+  (** [includes subsel s] returns true if the selector [s] includes the selector [subsel].
       I.e. if [subsel] matches a path p, [s] also matches p.  
       Note that only the path part of the selectors are considered for the inclusion (not the query and the fragment) *)
-  val is_prefixed_by_path : Path.t -> t -> bool
-  (** [is_prefixed_by_path p s] returns true if the selector [s] partially matches the path [p].
-      I.e. it exists a prefix of [s] that fully matches [p].
-      Note that only the path part of the selector is considered for the matching (not the query and the fragment) *)
-  val remove_matching_prefix : Path.t -> t -> t option
-  (** [remove_matching_prefix p s] checks if the Path [p] matches a substring prefixing the Selector [s].
+  val intersects : t -> t -> bool
+  (** intersect s1 s2] returns true if the intersection of selector [s1] and [s2] is not empty.
+      I.e. if it exists a path p that matches both expressions [s1] and [s2].
+      Note that only the path part of the selectors are considered for the inclusion (not the query and the fragment) *)
+  val remaining_after_match : Path.t -> t -> t option
+  (** [remaining_after_match p s] checks if the Path [p] matches a substring prefixing the Selector [s].
       If there is such matching prefix in [s], a similar Selector than [s] is returned, but with this prefix removed
       from its path part. If there is no such matching, None is returned. *)
 end [@@deriving show]
